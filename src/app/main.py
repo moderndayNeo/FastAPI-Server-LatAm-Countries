3a055364ad4c3c80820b05c2d7217f04f6e4ccf0
Adam Zdrzalka <57966028+moderndayNeo@users.noreply.github.com>
2025-04-06 17:08:24 +0200

    Add a /countries route

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,24 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Create an empty array named "countries"
+countries = [
+    {
+        "id": 1,
+        "name": "Colombia",
+        "capital": "Bogota",
+        "population": 50000000,
+
+    },
+    {
+        "id": 2,
+        "name": "Brazil",
+        "capital": "Brasilia",
+        "population": 211000000,
+    }
+]
+
+
+@app.get("/countries")
+def get_countries():
+    return countries
